51999183dc77e1380f59b6cc311e4716309f3fe0
Danny Parks <dparks2@live.maryville.edu>
2019-04-11 09:21:15 -0500

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
+import { SocialSharing } from '@ionic-native/social-sharing';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  
+  constructor(public navCtrl: NavController, public inputDialogService: InputDialogServiceProvider, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public socialSharing: SocialSharing) {
+
+  }
+
+
+  loadItems() {
+    return this.dataService.getItems();
+  }
+
+  removeItem(item, index) {
+    console.log("Removing Item -- ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item --' + index + "...",
+      duration: 3000});
+      toast.present();
+      this.dataService.removeItem(index);
+      
+  }
+  shareItem(item, index) {
+    console.log("sharing Item -- ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Sharing Item --' + index + "...",
+      duration: 3000});
+      toast.present();
+      
+      let message = "Grocery Item - Name : " + item.name + " - Quantity: " + item.quantity;
+      let subject = "Shared via Groceries app";
+      this.socialSharing.share(message, subject).then(() => {
+        console.log("Shared successfully!");
+      }).catch((error) => {
+        console.error("Error whie sharing", error);
+      });
+      
+  }
+
+
+  editItem(item, index) {
+    console.log("Edit Item -- ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Editing Item --' + index + "...",
+      duration: 3000});
+      toast.present();
+      this.inputDialogService.showPrompt(item,index);
+   
+  }
+
+
+  addItem() { 
+    console.log("Adding Item");
+    this.inputDialogService.showPrompt();
+  }
+
+ 
+
+
+}
+
